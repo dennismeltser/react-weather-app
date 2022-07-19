cbafa86a36583336c0090db05f6ce546b988e4b2
dennismeltser <dennis.meltser@gmail.com>
2022-07-19 16:24:07 -0600

    :

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '457110e1bcmshb12f7516d23bc29p144d91jsnb556f4dd9189',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+    },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
